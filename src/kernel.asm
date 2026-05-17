1177baca5861099c73f0b8f99bde4e449899eb36
mandyrin <mandyrin@citrus.com>
2026-05-17 10:57:24 +1000

    Fix typo

--- src/kernel.asm
+++ src/kernel.asm
@@ -3,6 +3,7 @@
 section .text               ; needs to be the first bit of code we run
 
 global _start
+extern kernel_main
 
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10 
@@ -21,6 +22,8 @@ _start:
     in al, 0x92
     or al, 2
     out 0x92, al
+
+    call kernel_main
     jmp $
 
 times 512-($-$$) db 0   ; solves alignment issues by forcing it to be 512 bytes long
--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,5 @@
 #include "kernel.h"
 
-void kernel_start() {
-    
+void kernel_main() {
+
 }
