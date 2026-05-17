fa1b58a3fe5654924159378cf56a9729ba5c1de0
mandyrin <mandyrin@citrus.com>
2026-05-17 23:15:38 +1000

    Make terminal char

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,13 @@
+#include <stdint.h>
+
 #include "kernel.h"
 
-void kernel_main() {
 
+uint16_t terminal_make_char(char c, char colour) {
+    return (colour << 8) | c;
+}
+
+void kernel_main() {
+    uint16_t* video_mem = (uint16_t*)(0xB8000);
+    video_mem[0] = terminal_make_char('B', 3);
 }
